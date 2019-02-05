685de2194d7cce96c73ab581c4928e4709e9da7c
hsdinc <dinchazalsu@gmail.com>
2019-02-05 12:56:48 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/03gua8igjlu4dif/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/hkszup073dakq6z/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
 classes = ['armadillos', 'echidnas', 'hedgehogs']
